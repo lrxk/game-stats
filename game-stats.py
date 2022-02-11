231a9899bedec029196d36db593ee0c7060d54c8
kevin leroux <kevin.leroux@etu.univ-lehavre.fr>
2022-02-11 16:39:29 +0100

    Add a few arguments

--- game-stats.py
+++ game-stats.py
@@ -7,8 +7,10 @@ class ArgumentException(Exception):
         self.message=message
         
 class GameStats:
+    ID="id"
     PLAYERS="players"
     MAP_NAME="map_name"
+    PODIUM="podium"
     def __init__(self):
         parser=argparse.ArgumentParser()
         parser.add_argument('-p','--players',help='Enter players and their positions',type=str,metavar='')
@@ -19,15 +21,29 @@ class GameStats:
         if args.players=='':
             raise ArgumentException('Players and their positions needed')
         self.data=json.loads(args.players)
+        self.map_name=args.map
     
+    # function to return key for any value
+    def get_key(self,val):
+        for key, value in self.data.items():
+            if val == value:
+                return key
+ 
+        return "key doesn't exist"
     def dataToJSON(self):
         now = datetime.now()
         dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+        first=self.get_key(1)
+        second=self.get_key(2)
+        third=self.get_key(3)
+        podium={"first":first,"second":second,"third":third}
         with open('result.json','r+') as file:
             jsonList=[]
             entry={}
-            entry['id']=dt_string
-            entry['players']=self.data
+            entry[self.ID]=dt_string
+            entry[self.PLAYERS]=self.data
+            entry[self.MAP_NAME]=self.map_name
+            entry[self.PODIUM]=podium
             #empty file
             if os.path.getsize("result.json")==0:
                 jsonList=[entry]
