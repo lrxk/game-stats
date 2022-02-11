7180e646fc303c42d109211611ded8f86d2cb312
kevin leroux <kevin.leroux@etu.univ-lehavre.fr>
2022-02-11 14:02:17 +0100

    argument function

--- game-stats.py
+++ game-stats.py
@@ -1,10 +1,17 @@
 import argparse
 import json
+from turtle import TPen
+
+from click import argument
 class GameStats:
     PLAYERS="players"
     MAP_NAME="map_name"
     def __init__(self) -> None:
-
+        args=self.argument()
         pass
-    def argument():
-        parser
+    def argument(self):
+        parser=argparse.ArgumentParser()
+        parser.add_argument('-p','--players',help='Enter players and their positions',type=str,metavar='')
+        parser.add_argument('-m','--map',help='Enter the map',type=str,metavar='')
+        args=parser.parse_args()
+        return args
