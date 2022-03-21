6d3eebab9fd491a18bc8c46d7149bff9b5be88cb
KEVIN LEROUX <kevin.leroux@etu.univ-lehavre.fr>
2022-03-21 11:40:25 +0100

    self argument

--- game-stats-gui.py
+++ game-stats-gui.py
@@ -15,27 +15,27 @@ class App:
         root.geometry(alignstr)
         root.resizable(width=False, height=False)
 
-        nb_Players_Entry=tk.Entry(root)
-        nb_Players_Entry["borderwidth"] = "1px"
+        self.nb_Players_Entry=tk.Entry(root)
+        self.nb_Players_Entry["borderwidth"] = "1px"
         ft = tkFont.Font(family='Times',size=10)
-        nb_Players_Entry["font"] = ft
-        nb_Players_Entry["fg"] = "#333333"
-        nb_Players_Entry["justify"] = "center"
-        nb_Players_Entry["text"] = "Nb_Players"
-        nb_Players_Entry.place(x=150,y=70,width=70,height=25)
+        self.nb_Players_Entry["font"] = ft
+        self.nb_Players_Entry["fg"] = "#333333"
+        self.nb_Players_Entry["justify"] = "center"
+        self.nb_Players_Entry["text"] = "Nb_Players"
+        self.nb_Players_Entry.place(x=150,y=70,width=70,height=25)
 
-        validator_1=tk.Button(root)
-        validator_1["bg"] = "#efefef"
+        self.validator_1=tk.Button(root)
+        self.validator_1["bg"] = "#efefef"
         ft = tkFont.Font(family='Times',size=10)
-        validator_1["font"] = ft
-        validator_1["fg"] = "#000000"
-        validator_1["justify"] = "center"
-        validator_1["text"] = "Ok"
-        validator_1.place(x=260,y=70,width=70,height=25)
-        validator_1["command"] = self.validator_1_command
+        self.validator_1["font"] = ft
+        self.validator_1["fg"] = "#000000"
+        self.validator_1["justify"] = "center"
+        self.validator_1["text"] = "Ok"
+        self.validator_1.place(x=260,y=70,width=70,height=25)
+        self.validator_1["command"] = self.validator_1_command
 
     def validator_1_command(self):
-        print("command")
+        pass
 
 if __name__ == "__main__":
     root = tk.Tk()
