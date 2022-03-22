1546276209440e11267c84aa022d5b32e8b67c91
KEVIN LEROUX <kevin.leroux@etu.univ-lehavre.fr>
2022-03-22 09:34:19 +0100

    Global variable and player and their score entries

--- game-stats-gui.py
+++ game-stats-gui.py
@@ -7,11 +7,11 @@ class App:
         #setting title
         root.title("undefined")
         #setting window size
-        width=600
-        height=500
+        self.width=600
+        self.height=500
         screenwidth = root.winfo_screenwidth()
         screenheight = root.winfo_screenheight()
-        alignstr = '%dx%d+%d+%d' % (width, height, (screenwidth - width) / 2, (screenheight - height) / 2)
+        alignstr = '%dx%d+%d+%d' % (self.width, self.height, (screenwidth - self.width) / 2, (screenheight - self.height) / 2)
         root.geometry(alignstr)
         root.resizable(width=False, height=False)
         vcmd = (root.register(self.callback))
@@ -34,21 +34,32 @@ class App:
         self.validator_1.place(x=260,y=70,width=70,height=25)
         self.validator_1["command"] = self.validator_1_command
         self.players_entries=[]
+        self.score_entries=[]
+        
     def callback(self, P):
         if str.isdigit(P) or P == "":
             return True
         else:
             return False
+    def reset(self):
+        for i in range(len(self.players_entries)):
+            self.players_entries[i].destroy()
+            self.score_entries[i].destroy()
+
+
     def validator_1_command(self):
+        self.reset()
         count=int(self.nb_Players_Entry.get())
+        yPlacement=110
         if count>self.MAX_ENTRIES:
             raise Exception("Can only be "+str(self.MAX_ENTRIES)+" entries")
         for i in range(count):
             self.players_entries.append(tk.Entry(root))
-            
-    
-
-
+            self.score_entries.append(tk.Entry(root))
+        for i in range(count):
+            self.players_entries[i].place(x=self.width/4,y=yPlacement,width=70,height=25)
+            self.score_entries[i].place(x=self.width/4+80,y=yPlacement,width=70,height=25)
+            yPlacement+=50
 if __name__ == "__main__":
     root = tk.Tk()
     app = App(root)
