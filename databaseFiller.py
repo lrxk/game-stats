c6d1306fc398f602f9b7f476985460291161f639
Kevin Leroux <k.lrx076@gmail.com>
2022-07-12 17:13:07 +0200

    Modify the types of the table game_stats_result, create a table containing the maps of marioKart 8, fill the table with game_stats from map_names

--- databaseFiller.py
+++ databaseFiller.py
@@ -4,6 +4,7 @@ class DatabaseFiller:
     def __init__(self) -> None:
         self.db_filename="game_stats.db"
         self.create_connection()
+        self.create__map_name_table()
         self.create_table()
         pass
     def create_connection(self):
@@ -17,9 +18,30 @@ class DatabaseFiller:
         finally:
             if conn:
                 conn.close()
+    def create__map_name_table(self):
+        conn = sqlite3.connect(self.db_filename)
+        c = conn.cursor()
+        c.execute("""CREATE TABLE IF NOT EXISTS map_name (
+                                                        id integer PRIMARY KEY,
+                                                        map_name text NOT NULL,
+                                                        original_console text NOT NULL,
+                                                        )""")
+        # if the table map_name is empty, insert the data from the Mario_kart8deluxe_maps_with_dlcs_map.csv file
+        if c.execute("""SELECT COUNT(*) FROM map_name""").fetchone()[0] == 0:
+            # read a csv file and insert the data in the table map_name
+            with open('Mario_kart8deluxe_maps_with_dlcs_map.csv','r') as file:
+                for line in file:
+                    line=line.split(',')
+                    c.execute("INSERT INTO map_name (map_name,original_console) VALUES (?,?)",(line[0],line[1]))
+                    
+        conn.commit()
+        conn.close()
     def create_table(self):
         conn = sqlite3.connect(self.db_filename)
         c = conn.cursor()
+        # create a table with the data from the Mario_kart8deluxe_maps_with_dlcs_map.csv file
+        
+        
         c.execute("""CREATE TABLE IF NOT EXISTS game_stats (
                                                         id integer PRIMARY KEY,
                                                         map_name text NOT NULL,
@@ -30,7 +52,7 @@ class DatabaseFiller:
         c.execute("""CREATE TABLE IF NOT EXISTS game_stats_result ( 
                                                         id integer PRIMARY KEY,
                                                         players text NOT NULL,
-                                                        scores text NOT NULL,
+                                                        scores INTEGER NOT NULL,
                                                         FOREIGN KEY (id) REFERENCES game_stats(id)
                                                         )""")
         conn.commit()
@@ -38,7 +60,8 @@ class DatabaseFiller:
     def insert_data(self,map_name,players,scores,date):
         conn = sqlite3.connect(self.db_filename)
         c = conn.cursor()
-        c.execute("INSERT INTO game_stats (map_name,date) VALUES (?,?)",(map_name,date))
+        for i in range(len(map_name)):
+            c.execute("INSERT INTO game_stats (map_name,date) VALUES (?,?)",(map_name[i],date))
         # insert the data of the score of each player in the table game_stats_result
         for i in range(len(players)):
             c.execute("INSERT INTO game_stats_result (players,scores) VALUES (?,?)",(players[i],scores[i]))
